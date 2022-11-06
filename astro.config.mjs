c775a87d692c83487d1b4011bce6bee99c98f186
Simon Vrachliotis <simon@simonswiss.com>
2022-11-06 12:54:03 +1100

    configure domain for SEO image, sitemap etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), partytown()]
-});
+  site: 'https://simonswiss.com',
+  integrations: [mdx(), sitemap(), partytown()],
+})
