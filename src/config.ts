31a77fe0a2a05df22182da3febed371cbe44c73c
Simon Vrachliotis <simon@simonswiss.com>
2022-11-07 08:56:15 +1100

    new social image + Pro Tailwind page gradient more surfy

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'simonswiss | Web designer, developer & content creator'
 export const SITE_DESCRIPTION = 'Swiss front end designer and developer based in Sydney, Australia.'
-export const SEO_IMAGE = '/social-large.jpg'
+export const SEO_IMAGE = '/social-large.png'
