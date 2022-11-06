79e6fb3dc953f726ad6b3175b6ab92e222ffb3c6
Simon Vrachliotis <simon@simonswiss.com>
2022-11-06 13:23:32 +1100

    astro-imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 import partytown from '@astrojs/partytown'
@@ -8,5 +9,5 @@ import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://simonswiss.com',
-  integrations: [mdx(), sitemap(), partytown()],
+  integrations: [mdx(), sitemap(), partytown(), astroImageTools],
 })
