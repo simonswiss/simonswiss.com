a96be2df9bc75e544ccc8afa30687e730dfeb294
Simon Vrachliotis <simon@simonswiss.com>
2022-11-06 13:54:46 +1100

    Alt GA implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 
 import { astroImageTools } from 'astro-imagetools'
 
 export default defineConfig({
   site: 'https://simonswiss.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-    astroImageTools,
-  ],
+  integrations: [mdx(), sitemap(), astroImageTools],
 })
