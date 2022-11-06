cc57944f0c57a875464c5a095dd64914fd8e3193
Simon Vrachliotis <simon@simonswiss.com>
2022-11-06 12:44:54 +1100

    Partytown + GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-})
+  integrations: [mdx(), sitemap(), partytown()]
+});
