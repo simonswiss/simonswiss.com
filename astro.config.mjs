196855efbc394fea6a5426649cd3438b1b75f05a
Simon Vrachliotis <simon@simonswiss.com>
2022-11-06 13:43:28 +1100

    GA not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import { astroImageTools } from 'astro-imagetools'
-
-// https://astro.build/config
 import partytown from '@astrojs/partytown'
 
-// https://astro.build/config
+import { astroImageTools } from 'astro-imagetools'
+
 export default defineConfig({
   site: 'https://simonswiss.com',
-  integrations: [mdx(), sitemap(), partytown(), astroImageTools],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    astroImageTools,
+  ],
 })
