55f71d4381bef9f58a391b77647255ef0dcce80e
Simon Vrachliotis <simon@simonswiss.com>
2022-11-09 16:11:07 +1100

    Support for pagination with global PAGINATION_COUNT config

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = 'simonswiss | Web designer, developer & content creator'
 export const SITE_DESCRIPTION = 'Swiss front end designer and developer based in Sydney, Australia.'
 export const SEO_IMAGE = '/social-large.png'
+
+export const PAGINATION_COUNT = 10
