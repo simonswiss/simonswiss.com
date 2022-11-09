c8ac55183f4c3453b7148a81960d9cc2f7f98ab3
Simon Vrachliotis <simon@simonswiss.com>
2022-11-09 22:28:40 +1100

    generate robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
+import robotsTxt from 'astro-robots-txt'
 import { astroImageTools } from 'astro-imagetools'
 
 export default defineConfig({
   site: 'https://simonswiss.com',
-  integrations: [mdx(), sitemap(), astroImageTools],
+  integrations: [mdx(), sitemap(), robotsTxt(), astroImageTools],
 })
