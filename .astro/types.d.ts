0b4c4493e4b32395ac7325ef43e31be1b004e5c6
Simon Vrachliotis <simon@simonswiss.com>
2023-04-10 21:18:46 +1000

    Stream with KCD page

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -155,6 +155,13 @@ declare module 'astro:content' {
   collection: "screencasts",
   data: any
 } & { render(): Render[".mdx"] },
+"live-stream-with-kent-c-dodds/index.mdx": {
+  id: "live-stream-with-kent-c-dodds/index.mdx",
+  slug: "live-stream-with-kent-c-dodds",
+  body: string,
+  collection: "screencasts",
+  data: any
+} & { render(): Render[".mdx"] },
 "rebuilding-instagram-stories-with-tailwind/index.mdx": {
   id: "rebuilding-instagram-stories-with-tailwind/index.mdx",
   slug: "rebuilding-instagram-stories-with-tailwind",
